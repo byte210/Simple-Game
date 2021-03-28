03f3511af9257b4307044c1796fe2ba9fe4a1eee
byte210 <62095430+byte210@users.noreply.github.com>
2021-03-28 15:31:57 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+function startGame(){
+    window.location = './level.html';
+}
+
+function goBack(){
+    window.location = './index.html';
+}
+
+function easy(){
+    window.location = './easyLevel.html';
+}
+
+function medium(){
+    window.location = './mediumLevel.html';
+}
+
+function hard(){
+    window.location = './hardLevel.html';
+}
+// easy level build
